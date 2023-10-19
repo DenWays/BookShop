d50a210a55a24cba40c7982a45600961562b5d81
DenWays <130283554+DenWays@users.noreply.github.com>
2023-10-19 21:03:54 +0500

    Create FromAuthorization. Frontend Form

---
+++ BookShopBD/FormAuthorizathion.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace BookShopBD
+{
+    partial class FormAuthorizathion
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAuthorizathion));
+            this.authorizationLabel = new System.Windows.Forms.Label();
+            this.loginTB = new System.Windows.Forms.TextBox();
+            this.loginLabel = new System.Windows.Forms.Label();
+            this.passwordTB = new System.Windows.Forms.TextBox();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.createAccountLabel = new System.Windows.Forms.Label();
+            this.authorizationButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panelAuthorization = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panelAuthorization.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // authorizationLabel
+            // 
+            this.authorizationLabel.AutoSize = true;
+            this.authorizationLabel.BackColor = System.Drawing.Color.Transparent;
+            this.authorizationLabel.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.authorizationLabel.ForeColor = System.Drawing.Color.White;
+            this.authorizationLabel.Location = new System.Drawing.Point(106, 53);
+            this.authorizationLabel.Name = "authorizationLabel";
+            this.authorizationLabel.Size = new System.Drawing.Size(185, 38);
+            this.authorizationLabel.TabIndex = 0;
+            this.authorizationLabel.Text = "Авторизация";
+            this.authorizationLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // loginTB
+            // 
+            this.loginTB.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.loginTB.Location = new System.Drawing.Point(100, 140);
+            this.loginTB.Name = "loginTB";
+            this.loginTB.Size = new System.Drawing.Size(200, 45);
+            this.loginTB.TabIndex = 1;
+            // 
+            // loginLabel
+            // 
+            this.loginLabel.AutoSize = true;
+            this.loginLabel.BackColor = System.Drawing.Color.Transparent;
+            this.loginLabel.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.loginLabel.ForeColor = System.Drawing.Color.White;
+            this.loginLabel.Location = new System.Drawing.Point(95, 108);
+            this.loginLabel.Name = "loginLabel";
+            this.loginLabel.Size = new System.Drawing.Size(76, 29);
+            this.loginLabel.TabIndex = 2;
+            this.loginLabel.Text = "Логин";
+            // 
+            // passwordTB
+            // 
+            this.passwordTB.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.passwordTB.Location = new System.Drawing.Point(100, 226);
+            this.passwordTB.Name = "passwordTB";
+            this.passwordTB.Size = new System.Drawing.Size(200, 45);
+            this.passwordTB.TabIndex = 3;
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.BackColor = System.Drawing.Color.Transparent;
+            this.passwordLabel.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.passwordLabel.ForeColor = System.Drawing.Color.White;
+            this.passwordLabel.Location = new System.Drawing.Point(95, 188);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(90, 29);
+            this.passwordLabel.TabIndex = 4;
+            this.passwordLabel.Text = "Пароль";
+            // 
+            // createAccountLabel
+            // 
+            this.createAccountLabel.AutoSize = true;
+            this.createAccountLabel.BackColor = System.Drawing.Color.Transparent;
+            this.createAccountLabel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.createAccountLabel.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.createAccountLabel.ForeColor = System.Drawing.Color.White;
+            this.createAccountLabel.Location = new System.Drawing.Point(96, 274);
+            this.createAccountLabel.Name = "createAccountLabel";
+            this.createAccountLabel.Size = new System.Drawing.Size(141, 23);
+            this.createAccountLabel.TabIndex = 5;
+            this.createAccountLabel.Text = "Создать аккаунт?";
+            this.createAccountLabel.MouseEnter += new System.EventHandler(this.createAccountLabel_MouseEnter);
+            this.createAccountLabel.MouseLeave += new System.EventHandler(this.createAccountLabel_MouseLeave);
+            // 
+            // authorizationButton
+            // 
+            this.authorizationButton.BackColor = System.Drawing.Color.White;
+            this.authorizationButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.authorizationButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.authorizationButton.FlatAppearance.BorderSize = 0;
+            this.authorizationButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.authorizationButton.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.authorizationButton.Location = new System.Drawing.Point(100, 300);
+            this.authorizationButton.Name = "authorizationButton";
+            this.authorizationButton.Size = new System.Drawing.Size(200, 45);
+            this.authorizationButton.TabIndex = 6;
+            this.authorizationButton.Text = "Авторизоваться";
+            this.authorizationButton.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::BookShopBD.Properties.Resources.bookWhite;
+            this.pictureBox1.Location = new System.Drawing.Point(159, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(78, 47);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panelAuthorization
+            // 
+            this.panelAuthorization.Controls.Add(this.pictureBox1);
+            this.panelAuthorization.Controls.Add(this.authorizationButton);
+            this.panelAuthorization.Controls.Add(this.createAccountLabel);
+            this.panelAuthorization.Controls.Add(this.passwordLabel);
+            this.panelAuthorization.Controls.Add(this.passwordTB);
+            this.panelAuthorization.Controls.Add(this.loginLabel);
+            this.panelAuthorization.Controls.Add(this.loginTB);
+            this.panelAuthorization.Controls.Add(this.authorizationLabel);
+            this.panelAuthorization.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelAuthorization.Location = new System.Drawing.Point(0, 0);
+            this.panelAuthorization.Name = "panelAuthorization";
+            this.panelAuthorization.Size = new System.Drawing.Size(384, 361);
+            this.panelAuthorization.TabIndex = 0;
+            this.panelAuthorization.Paint += new System.Windows.Forms.PaintEventHandler(this.panelAuthorization_Paint);
+            // 
+            // FormAuthorizathion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(60)))), ((int)(((byte)(69)))));
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.panelAuthorization);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "FormAuthorizathion";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Book Shop";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panelAuthorization.ResumeLayout(false);
+            this.panelAuthorization.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label authorizationLabel;
+        private System.Windows.Forms.TextBox loginTB;
+        private System.Windows.Forms.Label loginLabel;
+        private System.Windows.Forms.TextBox passwordTB;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.Label createAccountLabel;
+        private System.Windows.Forms.Button authorizationButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panelAuthorization;
+    }
+}
+
---
+++ BookShopBD/FormAuthorizathion.cs
@@ -0,0 +1,35 @@
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace BookShopBD
+{
+    public partial class FormAuthorizathion : Form
+    {
+        public FormAuthorizathion()
+        {
+            InitializeComponent();
+        }
+
+        private void panelAuthorization_Paint(object sender, PaintEventArgs e)
+        {
+            using (LinearGradientBrush brush = new LinearGradientBrush(this.ClientRectangle,
+                                                                       Color.FromArgb(47, 60, 69),
+                                                                       Color.FromArgb(240, 71, 44),
+                                                                       90))
+            {
+                e.Graphics.FillRectangle(brush, this.ClientRectangle);
+            }
+        }
+
+        private void createAccountLabel_MouseEnter(object sender, System.EventArgs e)
+        {
+            createAccountLabel.ForeColor = Color.Blue;         
+        }
+
+        private void createAccountLabel_MouseLeave(object sender, System.EventArgs e)
+        {
+            createAccountLabel.ForeColor = Color.White;
+        }
+    }
+}
